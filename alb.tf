bfb3be1b64e11b626fcab52030e402f1341c82a6
surendra <surendraredddyalamuru01@gmail.com>
2024-04-24 15:12:28 +0530

    apps

--- alb.tf
+++ alb.tf
@@ -59,3 +59,20 @@ resource "aws_lb_listener" "main" {
     }
   }
 }
+
+resource "aws_lb_listener" "public-http" {
+  count             = var.internal ? 0 : 1
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
